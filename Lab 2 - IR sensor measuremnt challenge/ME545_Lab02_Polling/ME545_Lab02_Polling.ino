3490287d656361fdb1a5fc6bc07ce187752987c2
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-02-09 19:33:27 -0500

    Feat: polling loop to sample IR sensor at certain frequency

---
+++ Lab 2 - IR sensor measuremnt challenge/ME545_Lab02_Polling/ME545_Lab02_Polling.ino
@@ -0,0 +1,46 @@
+unsigned long startTime = 0; // variable for the start time
+unsigned long currentTime = 0; // variable for the current time
+unsigned long elapsedTime; // variable to store the elapsed time (currentTime - startTime)
+unsigned long samplingTime;
+
+float IRsensorRaw = A0; // set raw IRsensor data as pin A0
+float IRsensorAF = A1; // set analog filtered IRsensor data as pin A1
+
+int count = 0; // initialize a counter
+
+float sensorValueRaw; // variable to store raw output of IR sensor
+float voltageRaw; // variable to store raw voltage of IR sensor
+
+float sensorValueAF; // variable to store analog filtered output of IR sensor
+float voltageAF; // variable to store analog filtered voltage of IR sensor
+
+void setup() {
+  Serial.begin(115200); // initialize the serial monitor
+  currentTime = millis(); // set the currentTime equal to millis function
+}
+
+void loop() {
+  currentTime = millis();
+  elapsedTime = currentTime - startTime; // calculate the elapsed time
+
+  // collect 200 sampling points
+  if (3 <= elapsedTime && 805 >= count) {
+    sensorValueRaw = analogRead(IRsensorRaw); // read the output of the IRsensor and store it
+    voltageRaw = sensorValueRaw * (5.0 / 1023.0); // convert the sensorValue to a voltage
+
+    sensorValueAF = analogRead(IRsensorAF); // read analog filtered output of IR sensor and store it
+    voltageAF = sensorValueAF * (5.0 / 1023.0); // convert filtered sensor value to voltage
+
+    Serial.print(count); // print 'count' to the serial monitor
+    Serial.print(", ");
+    Serial.print(currentTime); // Print data to serial monitor
+    Serial.print(",");
+    Serial.print(voltageRaw); // print raw voltage data to the serial monitor
+    Serial.print(", ");
+    Serial.println(voltageAF); // print filtered voltage data
+
+    
+    startTime = millis();
+    count += 1; // add 1 to variable 'count'
+  }
+}
