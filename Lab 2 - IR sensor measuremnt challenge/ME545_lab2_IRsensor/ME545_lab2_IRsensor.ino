4059e82a1712dd57fd053f0688b8db462aef5051
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-02-11 11:58:52 -0500

    Fix: cleaned up and fully commented

--- Lab 2 - IR sensor measuremnt challenge/ME545_lab2_IRsensor/ME545_lab2_IRsensor.ino
+++ Lab 2 - IR sensor measuremnt challenge/ME545_lab2_IRsensor/ME545_lab2_IRsensor.ino
@@ -1,6 +1,6 @@
 /*
    ME 545 - Mechatronics
-   Lab 2 - IR Sensors
+   Lab 2 - IR Sensors final code
    Lab Group 5
    Juliette Mitrovich, Sijani, Sheila Moroney
 */
@@ -10,17 +10,22 @@ unsigned long currentTime = 0; // variable for the current time
 unsigned long elapsedTime; // variable to store the elapsed time (currentTime - startTime)
 
 // IR sensor 1 variables
-float IRsensor1 = A1; // set IRsensor as analog pin 0
+float IRsensor1 = A0; // set IRsensor as analog pin 0
 float sensorValue1; // variable to store the output of IR sensor 1
 
 // IR sensor 2 variables
-float IRsensor2 = A0; // set IRsensor as analog pin 0
+float IRsensor2 = A1; // set IRsensor as analog pin 0
 float sensorValue2; // variable to store the output of IR sensor 1
 
-// digital filter coefficients, both sensors
-float a1 = -0.6314;
-float b0 = 0.1843;
-float b1 = 0.1843;
+// digital filter coefficients, sensor 1 (fc = 27.77Hz, fs = 125Hz )
+float a11 = -0.0875;
+float b01 = 0.4563;
+float b11 = 0.4563;
+
+// digital filter coefficients, sensor 2 (fc = 22.22Hz, fs = 125Hz)
+float a12 = -0.2309;
+float b02 = 0.3846;
+float b12 = 0.3846;
 
 // digital filter, IR sensor 1 values
 float Vold1 = 0; // initialize the first input
@@ -33,10 +38,10 @@ float yold2 = 0; // initialize the first output
 float digitalFilter2; // initialize variable to store first order digital filter values
 
 // variables to store distance calculations
-float distance1; // store distance variables
-float distance2;
+float distance1; // store distance calculation from IR sensor 1
+float distance2; // store distance calculation from IR sensor 2
 float distance2_adj; // adjusted distance from sensor 2, set 20cm back
-float distance;
+float distance; // final distance calculated from both IR sensors
 
 // weight variables for sensor fusion
 float w1;
@@ -56,8 +61,8 @@ void loop() {
   currentTime = millis();
   elapsedTime = currentTime - startTime; // calculate the elapsed time
 
-  // collect 200 sampling points
-  if (3 <= elapsedTime) {
+  // collect data at a fequency of 125 Hz (every 8 milliseconds)
+  if (8 <= elapsedTime) {
 
     // Collect IR sensor 1 data
     sensorValue1 = analogRead(IRsensor1);
@@ -65,35 +70,37 @@ void loop() {
     // Collect IR sensor 2 data
     sensorValue2 = analogRead(IRsensor2);
 
-    // collect digitally filtered data IR sensor 1
-    digitalFilter1 = (b0 * sensorValue1) + (b1 * Vold1) - (a1 * yold1);
+    // digitially filter IR sensor 1 data
+    digitalFilter1 = (b01 * sensorValue1) + (b11 * Vold1) - (a11 * yold1);
     yold1 = digitalFilter1;
     Vold1 = sensorValue1;
 
-    digitalFilter2 = (b0 * sensorValue2) + (b1 * Vold2) - (a1 * yold2);
+    // digitally filter IR sensor 2 data
+    digitalFilter2 = (b02 * sensorValue2) + (b12 * Vold2) - (a12 * yold2);
     yold2 = digitalFilter2;
     Vold2 = sensorValue2;
 
     // Convert filtered IR sensor data to distance
     distance1 = (16569.0 / (digitalFilter1 + 25)) - 11;
     distance2 = (16569.0 / (digitalFilter2 + 25)) - 11;
-    distance2_adj = distance2 - 20;
+    distance2_adj = distance2 - 21;
 
-    // Sensor Fusion
-    if (135 <= distance2) {
-      w1 = 1.0435;
+    // Use two distances to calculate the final distance
+    // if IR sensor 2 reads a distance of 65 or more, only use IR sensor 1 data
+    if (65 < distance2) {
+      w1 = .935;
       w2 = 0;
-      distance = ((w1 * distance1) + (w2 * distance2_adj)) / (w1 + w2);
+      distance = (w1 * distance1);
     }
-
-    if (20 >= distance2_adj) {
+    // if IR sensor 2 reads a distance of 40cm or closer, only use IR sensor 2 data
+    if (40 >= distance2) {
       w1 = 0;
       w2 = 1;
       distance = ((w1 * distance1) + (w2 * distance2_adj)) / (w1 + w2);
     }
 
-    // maybe not 1 and 1, probably a little more on the worse sensor
-    else {
+    // if IR sensor 2 reads a distance between 40cm and 60 cm, average the two sensor distances
+    if (65 >= distance2 && 40 < distance2) {
       w1 = 1;
       w2 = 1;
       distance = ((w1 * distance1) + (w2 * distance2_adj)) / (w1 + w2);
@@ -103,13 +110,13 @@ void loop() {
     pitch = map(distance, 0, 150, 120, 1500);
     tone(9, pitch);
 
-    distance = ((w1 * (distance1) + (w2 * distance2_adj)) / (w1 + w2));
+    // Print data to the serial monitor
+    //      Serial.print(distance1);
+    //      Serial.print(", ");
+    //      Serial.println(distance2);
+    //      Serial.print(", ");
+    Serial.println(distance);
 
-    if (0 == (elapsedTime % 100)) {
-      Serial.println(distance);
-    }
-
-
-    startTime = millis();
+    startTime = millis(); // reset startTime to allow for 125Hz of polling
   }
 }
