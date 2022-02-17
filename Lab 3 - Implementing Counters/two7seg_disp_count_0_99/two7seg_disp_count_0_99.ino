2a71121ce08e002e7b537a58362953270a1e9171
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-02-17 15:01:56 -0500

    Feat: Two 7 segment displays, count 0-99

---
+++ Lab 3 - Implementing Counters/two7seg_disp_count_0_99/two7seg_disp_count_0_99.ino
@@ -0,0 +1,51 @@
+#include "SevSeg.h"
+SevSeg sevseg1;
+SevSeg sevseg2;
+
+//Set to 1 for single digit display, 1st display
+byte numDigits1 = 1;
+
+//defines common pins while using multi-digit display. Left empty as we have a single digit display
+byte digitPins1[] = {};
+
+//Defines arduino pin connections in order: A, B, C, D, E, F, G, DP
+byte segmentPins1[] = {3, 2, 8, 7, 6, 4, 5, 9};
+bool resistorsOnSegments1 = true;
+
+//Set to 1 for single digit display, 2nd display
+byte numDigits2 = 1;
+
+//defines common pins while using multi-digit display. Left empty as we have a single digit display
+byte digitPins2[] = {};
+
+//Defines arduino pin connections in order: A, B, C, D, E, F, G, DP
+byte segmentPins2[] = {33, 32, 38, 37, 36, 34, 35, 39};
+bool resistorsOnSegments2 = true;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  //Initialize sevseg object. Uncomment second line if you use common cathode 7 segment
+  sevseg2.begin(COMMON_ANODE, numDigits1, digitPins1, segmentPins1, resistorsOnSegments1);
+  sevseg1.begin(COMMON_ANODE, numDigits2, digitPins2, segmentPins2, resistorsOnSegments2);
+  //sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
+
+  sevseg1.setBrightness(90);
+  sevseg2.setBrightness(90);
+}
+
+void loop()
+{
+  //Display numbers one by one with 2 seconds delay
+  for (int i = 0; i <= 99; i++) {
+    int disp1 = i % 10;
+    int disp2 = i / 10;
+    sevseg1.setNumber(disp1);
+    sevseg2.setNumber(disp2);
+    sevseg1.refreshDisplay();
+    sevseg2.refreshDisplay();
+    delay(1000);
+  }
+
+}
