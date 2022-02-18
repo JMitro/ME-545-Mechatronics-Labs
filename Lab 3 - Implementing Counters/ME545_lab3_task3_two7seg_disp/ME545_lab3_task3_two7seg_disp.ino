693f7f5b2f6e476480e2cb453306fa3da97a2b8d
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-02-17 22:20:38 -0500

    FIx: Updated code

--- Lab 3 - Implementing Counters/ME545_lab3_task3_two7seg_disp/ME545_lab3_task3_two7seg_disp.ino
+++ Lab 3 - Implementing Counters/ME545_lab3_task3_two7seg_disp/ME545_lab3_task3_two7seg_disp.ino
@@ -1,3 +1,5 @@
+
+///// SEVEN SEGMENT DISPLAY VARIABLES /////
 #include "SevSeg.h"
 SevSeg sevseg1;
 SevSeg sevseg2;
@@ -22,30 +24,77 @@ byte digitPins2[] = {};
 byte segmentPins2[] = {33, 32, 38, 37, 36, 34, 35, 39};
 bool resistorsOnSegments2 = true;
 
+int i = 0;
+
+///// PUSH BUTTON VARIABLES /////
+int pushButtonEnable = 0;
+int flagEnable;
+int stateButtonEnable;
+
+int pushButtonClear = 0;
+int flagClear;
+int stateButtonClear;
+
+///// PHOTO INTERRUPTOR VARIABLES /////
+int photoInterrupt = A1;
+int photoInterruptVal;
+
+void ISR () {
+  if (flagEnable == 1) {
+    if (i <= 99) {
+      i++;
+      int disp1 = i % 10;
+      int disp2 = i / 10;
+      sevseg1.setNumber(disp1);
+      sevseg2.setNumber(disp2);
+      sevseg1.refreshDisplay();
+      sevseg2.refreshDisplay();
+    }
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+
   //Initialize sevseg object. Uncomment second line if you use common cathode 7 segment
   sevseg2.begin(COMMON_ANODE, numDigits1, digitPins1, segmentPins1, resistorsOnSegments1);
   sevseg1.begin(COMMON_ANODE, numDigits2, digitPins2, segmentPins2, resistorsOnSegments2);
   //sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
-
   sevseg1.setBrightness(90);
   sevseg2.setBrightness(90);
+
+  // Initialize push buttons
+  pinMode(pushButtonEnable, INPUT_PULLUP);
+  pinMode(pushButtonClear, INPUT_PULLUP);
+
+  pinMode(photoInterrupt, INPUT);
+  attachInterrupt(digitalPinToInterrupt(photoInterrupt), ISR, CHANGE);
 }
 
-void loop()
-{
-  //Display numbers one by one with 2 seconds delay
-  for (int i = 0; i <= 99; i++) {
-    int disp1 = i % 10;
-    int disp2 = i / 10;
-    sevseg1.setNumber(disp1);
-    sevseg2.setNumber(disp2);
-    sevseg1.refreshDisplay();
-    sevseg2.refreshDisplay();
-    delay(1000);
+void loop() {
+  stateButtonEnable = digitalRead(pushButtonEnable);
+  stateButtonClear = digitalRead(pushButtonClear);
+
+  // If the enable button is pushed, add one to the flag
+  if (stateButtonEnable == 0) {
+    flagEnable = flagEnable + 1;
   }
+  if (flagEnable == 1) {
+    sevseg1.setNumber(i);
+    sevseg2.setNumber(i);
+  }
+
+  if (stateButtonClear == 0) {
+    flagClear = flagClear + 1;
+  }
+
+  if (flagClear == 1) {
+    i = 0;
+    sevseg1.setNumber(i);
+    sevseg2.setNumber(i);
+  }
+
+
 
 }
