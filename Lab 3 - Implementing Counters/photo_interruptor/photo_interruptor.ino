5aff95512954b0c837531ceb394ea68bfe8b0e06
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-02-18 13:15:32 -0500

    Feat: Code to read an optical interruptor

---
+++ Lab 3 - Implementing Counters/photo_interruptor/photo_interruptor.ino
@@ -0,0 +1,29 @@
+/*
+   Code to read an optical interruptor
+   Built in LED on pin 13 will turn on when something is placed between the optical interrupt sensor
+*/
+
+int photoInterrupt = A0;
+int photoInterruptVal;
+int led = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode (led, OUTPUT);
+}
+
+void loop() {
+  photoInterruptVal = (analogRead(photoInterrupt));
+  Serial.println(photoInterruptVal);
+  
+  if (photoInterruptVal < 500)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
+  delay(250);
+
+}
